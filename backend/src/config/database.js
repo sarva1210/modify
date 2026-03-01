32dd7d739c5684d1f16497857b8bf21b05be701f
sarva1210 <sarva.vish77@gmail.com>
2026-03-01 14:05:34 +0530

    initialized

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 function connectToDB() {
     mongoose.connect(process.env.MONGO_URI)
-        .then(() => {
-            console.log("Connected to DB")
-        })
-        .catch(err => {
-            console.log("Error connecting to DB", err)
-        })
+
+    .then(() => {
+        console.log("Connected to DB")
+    })
+    .catch(err => {
+        console.log("Error connecting to DB", err)
+    })
 }
 
 module.exports = connectToDB;
