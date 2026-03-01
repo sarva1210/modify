9c5986df2f4158e4228cf8bc9a7a0a9d2622ebd6
sarva1210 <sarva.vish77@gmail.com>
2026-03-01 14:03:58 +0530

    initialized

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const blacklistModel = require('../models/blacklist.model')
+const redis = require('../config/cache')
 
 async function registerUser(req,res) {
     const { username, email, password } = req.body;
@@ -111,11 +112,13 @@ async function getMe(req,res) {
 async function logoutUser(req,res) {
     const token = req.cookies.token
     res.clearCookie('token')
-    await blacklistModel.create({
-        token
-    })
+
+    // token --->key ; Date.now().toString() --->value
+    await redis.set(token,Date.now().toString(),"EX", 60 * 60)
+
     res.status(200).json({
         message:'logout successfully'
     })
 }
+
 module.exports = { registerUser, loginUser, getMe, logoutUser }
