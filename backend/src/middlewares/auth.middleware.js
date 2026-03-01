6751550eca28f7b6bdba750e2b24a0c79837645e
sarva1210 <sarva.vish77@gmail.com>
2026-03-01 14:04:07 +0530

    initialized

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,17 +1,19 @@
 const blacklistModel = require('../models/blacklist.model')
 const userModel = require('../models/user.model')
+const redis = require('../config/cache')
 const jwt = require('jsonwebtoken')
 
 async function authUser(req,res,next) {
     const token = req.cookies.token;
+    
     if(!token){
         return res.status(401).json({
             message:"token not provided"
         })
     }
-    const isTokenBlacklisted = await blacklistModel.findOne({
-        token
-    })
+
+    const isTokenBlacklisted = await redis.get(token)
+
     if (isTokenBlacklisted) {
         return res.status(401).json({
             message: "Invalid token"
